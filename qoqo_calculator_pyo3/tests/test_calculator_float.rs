0309111b6c60da6891d451bb54957b0c17f85584
Dimitri Berthault <dimitri.berthault@quantumsimulations.de>
2024-04-19 16:54:40 +0200

    Update to pyo3 0.21

--- qoqo_calculator_pyo3/tests/test_calculator_float.rs
+++ qoqo_calculator_pyo3/tests/test_calculator_float.rs
@@ -5,16 +5,10 @@ fn test_initialising_calculator_float() {
     pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| {
         let python_type = py.get_type_bound::<CalculatorFloatWrapper>();
-        let new_result = python_type.call((1.0,), None).unwrap();
-        let float_value = f64::extract(
-            new_result
-                .downcast::<CalculatorFloatWrapper>()
-                .unwrap()
-                .as_gil_ref()
-                .call_method0("__float__")
-                .unwrap(),
-        )
-        .unwrap();
+        let binding = python_type.call((1.0,), None).unwrap();
+        let new_result = binding.downcast::<CalculatorFloatWrapper>().unwrap();
+        let float_value =
+            f64::extract(new_result.as_gil_ref().call_method0("__float__").unwrap()).unwrap();
         assert!((float_value - 1.0).abs() < f64::EPSILON);
     })
 }
